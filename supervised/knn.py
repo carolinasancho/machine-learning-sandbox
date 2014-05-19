4e588e02890417bf7109be5f5c9c35c0737a05bf
Vitor Campos de Oliveira <vitorcoliveira@gmail.com>
2014-05-19 11:10:18 -0300

    new dirs

--- supervised/knn.py
+++ supervised/knn.py
@@ -21,16 +21,21 @@ def norm(dataset):
     norm_dataset = norm_dataset/tile(ranges, (m, 1))
     return norm_dataset, ranges, min_vals
 
-def classify0(input, dataset, labels, k):
-    dataset_size = dataset.shape[0]
-    difference_matrix = np.tile(input, (dataset_size, 1)) - dataset
-    square_difference_matrix = difference_matrix**2
-    square_distances = square_difference_matrix.sum(axis=1)
-    distances = square_distances**0.5
-    sorted_distance_indices = distances.argsort()
-    class_count = {}
-    for i in range(k):
-        vote = labels[sorted_distance_indices[i]]
-        class_count[vote] = class_count.get(vote, 0) + 1
-    sorted_class_count = sorted(class_count.iteritems(), key=operator.itemgetter(1), reverse=True)
-    return sorted_class_count[0][0]
+class KNNClassifier:
+
+    def __init__(self, neighbors=5):
+        self.neighbors = neighbors
+
+    def classify(self, input, dataset, labels):
+        dataset_size = dataset.shape[0]
+        difference_matrix = np.tile(input, (dataset_size, 1)) - dataset
+        square_difference_matrix = difference_matrix**2
+        square_distances = square_difference_matrix.sum(axis=1)
+        distances = square_distances**0.5
+        sorted_distance_indices = distances.argsort()
+        class_count = {}
+        for i in range(self.neighbors):
+            vote = labels[sorted_distance_indices[i]]
+            class_count[vote] = class_count.get(vote, 0) + 1
+        sorted_class_count = sorted(class_count.iteritems(), key=operator.itemgetter(1), reverse=True)
+        return sorted_class_count[0][0]
