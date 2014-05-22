5296e8191eebd820d53c0ee1edb71814d431454a
Vitor Campos de Oliveira <vitorcoliveira@gmail.com>
2014-05-22 15:49:42 -0300

    trees

---
+++ pydisanto/supervised/trees.py
@@ -0,0 +1,68 @@
+from math import log
+from collections import defaultdict
+
+def calcShannonEntropy(dataset):
+    num_entries = len(dataset)
+    labels_counts = defaultdict(int)
+    for instance in dataset:
+        labels_counts[instance[-1]] += 1
+    shannonEntropy = 0.0
+    for label in labels_counts:
+        prob = float(labels_counts[label])/num_entries
+        shannonEntropy -= prob*log(prob,2)
+    return shannonEntropy
+
+# return a new dataset conditional to the axis and value
+def splitDataset(dataset, axis, value):
+    ret_dataset = []
+    for instance in dataset:
+        if instance[axis] == value:
+            reduced_instance = instance[:axis]
+            reduced_instance.extend(instance[axis+1:])
+            ret_dataset.append(reduced_instance)
+    return ret_dataset
+
+def chooseBestFeatureToSplit(dataset):
+    num_features = len(dataset[0]) - 1
+    base_entropy = calcShannonEntropy(dataset)
+    best_info_gain = 0.0
+    best_feature = -1
+    for i in xrange(num_features):
+        feature_list = [instance[i] for instance in dataset]
+        unique_vals = set(feature_list)
+        new_entropy = 0.0
+        for value in unique_vals:
+            sub_dataset = splitDataset(dataset, i, value)
+            prob = len(sub_dataset)/float(len(dataset))
+            new_entropy += prob*calcShannonEntropy(sub_dataset)
+        info_gain = base_entropy - new_entropy
+        if (info_gain > best_info_gain):
+            best_info_gain = info_gain
+            best_feature = i
+    return best_feature
+
+def test_splitDataset():
+    dataset = [[1, 1, 'yes'], [1, 1, 'yes'], [1, 0, 'no'], [0, 1, 'no'], [0, 1, 'no']]
+    labels = ['no surfacing', 'flippers']
+    print 'Original dataset:'
+    print dataset
+    print 'Spliting o feature 0 and value 1:'
+    print splitDataset(dataset, 0, 1)
+
+def test_calcShannonEntropy():
+    dataset = [[1, 1, 'yes'], [1, 1, 'yes'], [1, 0, 'no'], [0, 1, 'no'], [0, 1, 'no']]
+    labels = ['no surfacing', 'flippers']
+    print calcShannonEntropy(dataset)
+    print 'Effect of adding one more class to the entropy:'
+    dataset[0][-1] = 'maybe'
+    print calcShannonEntropy(dataset)
+
+def test_chooseBestFeatureToSplit():
+    dataset = [[1, 1, 'yes'], [1, 1, 'yes'], [1, 0, 'no'], [0, 1, 'no'], [0, 1, 'no']]
+    print 'Best feature to split:'
+    print chooseBestFeatureToSplit(dataset)
+
+if __name__ == '__main__':
+    #test_calcShannonEntropy()
+    #test_splitDataset()
+    test_chooseBestFeatureToSplit()
