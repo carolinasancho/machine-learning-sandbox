3cc5d3cc9ee8df48e0006e2f5b84d99ff63e5321
Vitor Campos de Oliveira <vitorcoliveira@gmail.com>
2016-04-10 16:26:33 -0300

    sklearn algorithms

--- pydisanto/experiments/visualtools.py
+++ pydisanto/experiments/visualtools.py
@@ -2,7 +2,8 @@ from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_decision_regions(X, y, classifier, resolution=0.02):
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
     # setup marker generator and color map
     markers = ('s', 'x', 'o', '^', 'v')
     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
@@ -12,7 +13,7 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
     x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
-                         np.arange(x2_min, x2_max, resolution))
+                           np.arange(x2_min, x2_max, resolution))
     Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
     Z = Z.reshape(xx1.shape)
     plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
@@ -24,3 +25,8 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
         plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
                     alpha=0.8, c=cmap(idx),
                     marker=markers[idx], label=cl)
+
+    if test_idx:
+        X_test, y_test = X[test_idx, :], y[test_idx]
+        plt.scatter(X_test[:, 0], X_test[:, 1], c='', alpha=1.0, linewidth=1,
+                    marker='o', s=55, label='test set')
